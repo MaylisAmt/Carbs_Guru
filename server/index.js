0c0abedb53cfc980ae40adbfbefc191dc58a40c2
Maylis <maylis.ameto@gmail.com>
2024-09-06 16:35:07 +0200

    [WIP] Display db data in frontend

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: '../.env'})
 import express from 'express';
+import cors from 'cors';
 import { Sequelize } from '@sequelize/core';
 import { PostgresDialect } from '@sequelize/postgres';
 import { QueryTypes } from 'sequelize';
@@ -8,6 +9,7 @@ import { QueryTypes } from 'sequelize';
 const app = express();
 //use json
 app.use(express.json());
+app.use(cors());
 
 const sequelize = new Sequelize({
     dialect: PostgresDialect,
@@ -39,6 +41,33 @@ app.get('/test', (req, res, next) => {
         next(err);
     }
 });
+
+app.get("/programs", async (req, res, next)=> {
+    const prog = await sequelize.query("SELECT * FROM Program", {
+        type: QueryTypes.SELECT,
+      });
+    try {
+        res.status(200).json({message: prog});
+    } catch(err) {
+        next(err);
+    }
+})
+
+
+app.post("/users", async (req, res) => {
+    const user = await utilisateur.create(
+        {
+            firstName: "name from back",
+            lastName : "lastname from back",
+            email : "email from back"
+        }
+    )
+    try {
+        res.status(200).json({message: "user successfully created"}, {created: user});
+    } catch(err) {
+        next(err);
+    }
+})
 
 
 const PORT = process.env.PORT || 3000;  // Utilisation d'une variable d'environnement pour le port
