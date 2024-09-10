37ab589eab9a9a0b061b12681d5d404ef1f0c680
Maylis <maylis.ameto@gmail.com>
2024-09-10 22:38:22 +0200

    [WIP] Style account page

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import icon from '../assets/icon.png'
+import './Account.css';
 
 const Account = () => {
   const [programs, setPrograms]=useState([ ])
@@ -17,7 +19,35 @@ const Account = () => {
   },[])
     return (
     <div>
-      <h1>My account</h1>
+      <h1>Profile</h1>
+      <img src={icon} alt="profile_icon" />
+      <p>Username</p>
+      <p className='goals_title'>My goals</p>
+      <div className='info'>
+        <div className='day_choice'>
+          <div className='day_icon'></div>
+          <div className='toggle'>
+            <input type="checkbox" id="switch" class="switch" />
+            <label for="switch" class="slider"></label>
+          </div>
+        </div>
+        <div className='prog_inputs'>
+          <div className='meal1'>
+            <p className='repas1'>Ici on prendra le nom du repas depuis la db</p>
+            <input type='number' className='protein_input'></input>
+            <input type='number' className='carbs_input'></input>
+            <input type='number' className='fat_input'></input>
+            <p>grammes</p>
+          </div>
+          <div className='meal2'></div>
+          <div className='meal3'></div>
+          <div className='meal4'></div>
+          <div className='meal5'></div>
+        </div>
+        <div className='save_button'>
+          <button type="submit" class="save-button">Save</button>
+        </div>
+      </div>
   <div className='programs'> 
 {programs.map(program => (
   <div className='program'> 
