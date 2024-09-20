9ceb03d95436d4ca855e382ba1a26791fc456d2b
Maylis <maylis.ameto@gmail.com>
2024-09-20 15:16:38 +0200

    [FEAT] Authentication backend reject unauthenticated requests

--- server/index.js
+++ server/index.js
@@ -5,33 +5,47 @@ import cors from 'cors';
 import { Sequelize } from '@sequelize/core';
 import { PostgresDialect } from '@sequelize/postgres';
 import { QueryTypes } from 'sequelize';
+import { initAuth } from '@propelauth/express'
 
 const app = express();
 //use json
 app.use(express.json());
 app.use(cors());
 
-const sequelize = new Sequelize({
-    dialect: PostgresDialect,
-    database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    host: 'db',
-    port: 5432
-  });
-
-try {
-    await sequelize.authenticate();
-    console.log("db is connected");
-} catch(error) {
-    console.log("Not connected");
-}
-
-const programs = await sequelize.query('SELECT * FROM Program', {
-  type: QueryTypes.SELECT,
+const {
+    requireUser,
+    fetchUserMetadataByUserId,
+} = initAuth({
+    authUrl: "https://1184511552.propelauthtest.com",
+    apiKey: "d2c0daa1d6699c898a5cd8e5b050c04e9efea617c679f3c5e4fbb38500fb1d973ca55ee249156a6cd77eb1f98cc5a588"
 });
 
-console.log("Programs: ", programs);
+
+app.get("/api/whoami", requireUser, (req, res) => {
+    res.status(200).json({message: `Hello user with ID + ${req.user.userId}`});
+});
+
+// const sequelize = new Sequelize({
+//     dialect: PostgresDialect,
+//     database: process.env.POSTGRES_DB,
+//     user: process.env.POSTGRES_USER,
+//     password: process.env.POSTGRES_PASSWORD,
+//     host: 'db',
+//     port: 5432
+//   });
+
+// try {
+//     await sequelize.authenticate();
+//     console.log("db is connected");
+// } catch(error) {
+//     console.log("Not connected");
+// }
+
+// const programs = await sequelize.query('SELECT * FROM Program', {
+//   type: QueryTypes.SELECT,
+// });
+
+// console.log("Programs: ", programs);
 
 //test api with error handling
 app.get('/test', (req, res, next) => {
@@ -42,16 +56,16 @@ app.get('/test', (req, res, next) => {
     }
 });
 
-app.get("/programs", async (req, res, next)=> {
-    const prog = await sequelize.query("SELECT * FROM Program", {
-        type: QueryTypes.SELECT,
-      });
-    try {
-        res.status(200).json({message: prog});
-    } catch(err) {
-        next(err);
-    }
-})
+// app.get("/programs", async (req, res, next)=> {
+//     const prog = await sequelize.query("SELECT * FROM Program", {
+//         type: QueryTypes.SELECT,
+//       });
+//     try {
+//         res.status(200).json({message: prog});
+//     } catch(err) {
+//         next(err);
+//     }
+// })
 
 
 app.post("/users", async (req, res) => {
